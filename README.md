76c4e51c216b84ab6b082e232995b0c9e37e4457
Wlodek Bzyl <matwb@univ.gda.pl>
2019-10-17 19:43:08 +0200

    Add 99 Bottles example

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ a range of verses, or the entire song.
 1. `verse(n)` – return the lyrics for the verse number n
 1. `verses(a, b)` – return the lyrics for verses numbered a through b
 1. `song` – return the lyrics for the entire song
+
+## Instalacja bibliotek
+
+```sh
+bundle install --path=.bundle
+```
+
+## Uruchamianie testów
+
+```sh
+ruby test/bottles_test.rb
+```
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -5,9 +5,39 @@ require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
 
-  def test_the_first_verse
-    expected = "..."
+   def test_the_first_verse
+    expected = "99 bottles of beer on the wall, " +
+    "99 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "98 bottles of beer on the wall.\n"
+
     assert_equal expected, Bottles.new.verse(99)
   end
 
+  def test_one_verse
+    expected = "1 bottle of beer on the wall, " \
+               "1 bottle of beer.\n" \
+               "Take it down and pass it around, " \
+               "no more bottles of beer on the wall.\n"
+
+    assert_equal expected, Bottles.new.verse(1)
+  end
+
+  def test_two_verse
+    expected = "2 bottles of beer on the wall, " \
+               "2 bottles of beer.\n" \
+               "Take one down and pass it around, " \
+               "1 bottle of beer on the wall.\n"
+
+    assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_another__verse
+    expected = "97 bottles of beer on the wall, " \
+               "97 bottles of beer.\n" \
+               "Take one down and pass it around, " \
+               "96 bottles of beer on the wall.\n"
+
+    assert_equal expected, Bottles.new.verse(97)
+end
 end
