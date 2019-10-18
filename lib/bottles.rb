59ed48f970e1807aea705a12364ae1aa0e2330bc
Wlodek Bzyl <matwb@univ.gda.pl>
2019-10-18 16:29:02 +0200

    Done with verse testing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,15 +1,20 @@
 class Bottles
-
   def verse(number)
-    if number == 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
+    case number
+    when 0
+      "No more bottles of beer on the wall, " \
+      "no more bottles of beer.\n" \
+      "Go to the store and buy some more, " \
+      "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " \
+      "1 bottle of beer.\n" \
+      "Take it down and pass it around, " \
       "no more bottles of beer on the wall.\n"
-    elsif number == 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
+    when 2
+      "2 bottles of beer on the wall, " \
+      "2 bottles of beer.\n" \
+      "Take one down and pass it around, " \
       "1 bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " \
@@ -18,5 +23,4 @@ class Bottles
       "#{number - 1} bottles of beer on the wall.\n"
     end
   end
-
 end
---
+++ remarks.md
@@ -0,0 +1,9 @@
+## pluralization
+
+Is it reasonable to tolerate a bit of duplication?
+
+1. Does the change in the code make it harder to understand?
+2. What is the future cost of doing nothing now?
+3. When will the future arrive, or how soon I will get more information?
+
+## what does it mean to write `if` rather than `case`?
